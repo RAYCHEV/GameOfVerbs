1fbcaa50b70321aa6a2a76e8b349e7f5e1510d0b
RAYCHEV <RAYCHEV@users.noreply.github.com>
2023-03-24 21:35:56 +0000

    GameOfVerbs2 init

---
+++ GameOfVerbs2/GameOfVerbs2/GameOfVerbs2App.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfVerbs2App.swift
+//  GameOfVerbs2
+//
+//  Created by Nikolay Raychev on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOfVerbs2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
